d4286a6ba3e95d6bf1142cd72a33f4fdf53c3c6a
Syahirah19 <syahirh.arsad@gmail.com>
2022-12-27 23:07:15 +0800

    Update with intend login>register>account
    Not yet update - main dashboard & update data to database

--- app/src/main/java/workshop/mobile/herocycle/Register.java
+++ app/src/main/java/workshop/mobile/herocycle/Register.java
@@ -2,20 +2,43 @@ package workshop.mobile.herocycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity {
 
     EditText edtEmail;
     EditText edtPassword;
-    EditText editTextPhone;
-    Button btnRegister;
+    EditText edtTextPhone;
+    Button btnContinue;
+
+
+    FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtTextPhone = findViewById(R.id.edtTextPhone);
+        btnContinue = findViewById(R.id.btnContinue);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = firebaseDatabase.getReference("User");
+
+        myRef.setValue("Hello World");
+
+    }
+
+    public void goAccount(View view) {
+        startActivity(new Intent(this, Account.class));
     }
 }
--- app/src/main/java/workshop/mobile/herocycle/loginsignup.java
+++ app/src/main/java/workshop/mobile/herocycle/loginsignup.java
@@ -2,13 +2,39 @@ package workshop.mobile.herocycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class loginsignup extends AppCompatActivity {
+
+    EditText edtEmail, edtPassword;
+    TextView txtRegister;
+    Button btn_Login;
+    ImageView imgEyeClose;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginsignup);
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        txtRegister = findViewById(R.id.txtRegister);
+
+        //    kalau nak gerak ke page lain
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(loginsignup.this, Register.class));
+            }
+        });
+
     }
 }
